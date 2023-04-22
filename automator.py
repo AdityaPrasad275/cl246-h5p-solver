590625307b33ff34b453050e09a1e81b04537247
TachoBlade <adityaNprasad275@gmail.com>
2023-04-22 16:25:18 +0530

    finding totalTime
    added a try except block for finding totaltime but isnt working

--- automator.py
+++ automator.py
@@ -9,7 +9,6 @@ url = input("Enter the Moodle lecture URL: ")
 username = input("Enter your Moodle username: ")
 password = input("Enter your Moodle password: ")
 
-
 # Open the browser and navigate to the Moodle login page
 driver = webdriver.Edge()
 driver.get(url)
@@ -29,14 +28,14 @@ login_button.click()
 
 delay = 5
 
+
+#switching to iframe
 try:
     # Wait for the element with the ID of h5p-iframe
-    wrapper = WebDriverWait(driver, delay).until(
+    iframe = WebDriverWait(driver, delay).until(
       EC.presence_of_element_located((By.CLASS_NAME, 'h5p-iframe'))
     )
     print("iframe is present in the DOM now")
-    # Find the iframe element by its ID or index
-    iframe = driver.find_element(By.CLASS_NAME, 'h5p-iframe')
         
     # Switch the driver's focus to the iframe
     driver.switch_to.frame(iframe)
@@ -44,10 +43,10 @@ except TimeoutException:
     print("iframe did not show up")
     exit()
 
-
+#extracting percentages from seekbar
 try:
     # Wait for the element with the xpath of h5p-content-interaction/seekbar 
-    wrapper = WebDriverWait(driver, delay).until(
+    seekbar = WebDriverWait(driver, delay).until(
       EC.presence_of_element_located((By.XPATH, '/html/body/div/div/div[3]/div[2]/div[4]'))
     )
     print("seekbar is present in the DOM now")
@@ -61,3 +60,49 @@ except TimeoutException:
     print("seekbar did not show up")
     exit()
 
+
+
+#finding total time
+try:
+    # Wait for the element with the ID of h5p-iframe
+    yt_iframe = WebDriverWait(driver, delay).until(
+      EC.presence_of_element_located((By.ID, 'h5p-youtube-0'))
+    )
+    print("yt iframe is present in the DOM now")
+
+    # Switch the driver's focus to the iframe
+    driver.switch_to.frame(yt_iframe)
+
+    # Wait for the element with the xpath of video 
+    video_stream = WebDriverWait(driver, delay).until(
+        EC.presence_of_element_located((By.XPATH, '/html/body/div/div/div[1]/video'))
+        )
+    print("video is present in the DOM now, finding total time")
+
+    totalTime = driver.execute_script("""
+        //const xpath = "/html/body/div/div/div[1]/video";
+        //const container = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+        //return container.duration
+        document.querySelectorAll('.video-stream')[0].duration
+    """)
+    print(totalTime)
+except TimeoutException:
+    print("video did not show up for measuring total time")
+    exit()
+
+
+# # iterating through array of percentages
+# for percentage in percentages:
+#     try:
+#         # Wait for the element with the xpath of video stream
+#         video_stream = WebDriverWait(driver, delay).until(
+#         EC.presence_of_element_located((By.XPATH, '/html/body/div/div/div[1]/video'))
+#         )
+#         print("video is present in the DOM now")
+
+#         percentages = driver.execute_script("""
+#             document.querySelectorAll('.video-stream')[0].currentTime = totalTime*{percentage} - 2
+#         """)
+#     except TimeoutException:
+#         print("seekbar did not show up")
+#         exit()
