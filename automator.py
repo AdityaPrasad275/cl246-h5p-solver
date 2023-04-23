85d94b2003efe25042553dd691846cd81eefbc01
TachoBlade <adityaNprasad275@gmail.com>
2023-04-23 14:44:24 +0530

    Multiple choice multiple correct questions done !
    The solving part is now fully done, just submit button remains and differentiating between mulitple choice multiple correct and other questions (possibly we could remove the time stamps/percentages which do not have mcmc questions but that would be skipping over so not exactly desirable)

--- automator.py
+++ automator.py
@@ -4,11 +4,16 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-import time
+import time	
 
-url = input("Enter the Moodle lecture URL: ")
-username = input("Enter your Moodle username: ")
-password = input("Enter your Moodle password: ")
+# url = input("Enter the Moodle lecture URL: ")
+# username = input("Enter your Moodle username: ")
+# password = input("Enter your Moodle password: ")
+
+
+url = "https://moodle.iitb.ac.in/mod/hvp/view.php?id=103577"
+username = "210020007"
+password = "isDivergenceOfElectric0?"
 
 # Open the browser and navigate to the Moodle login page
 driver = webdriver.Edge()
@@ -79,6 +84,7 @@ switchToFrame('h5p-iframe', "class")
 
 #extract percentages
 percentages = operation('/html/body/div/div/div[3]/div[2]/div[4]', 'seekbar', 'percentage_extractor')
+print(percentages)
 
 switchToFrame("h5p-youtube-0", "id")
 
@@ -101,20 +107,91 @@ try:
 except TimeoutException:
     print("video did not show up for measuring total time")
     exit()
+print(totalTime)
 
-# # iterating through array of percentages
-# for percentage in percentages:
-#     try:
-#         # Wait for the element with the xpath of video stream
-#         video_stream = WebDriverWait(driver, delay).until(
-#           EC.presence_of_element_located((By.XPATH, '/html/body/div/div/div[1]/video'))
-#         )
-#         print("video is present in the DOM now")
+driver.switch_to.default_content()
 
-#         percentages = driver.execute_script("""
-#             document.querySelectorAll('.video-stream')[0].currentTime = totalTime*{percentage} - 2
-#         """)
-#     except TimeoutException:
-#         print("seekbar did not show up")
-#         exit()
+#switching to h5p iframe
+switchToFrame('h5p-iframe', "class")
+
+# iterating through array of percentages
+for percentage in percentages:
+        
+    driver.switch_to.default_content()
+    switchToFrame('h5p-iframe', "class")
+    switchToFrame("h5p-youtube-0", "id")
+
+    # Wait for the element with the xpath of video stream
+    video_stream = WebDriverWait(driver, delay).until(
+        EC.presence_of_element_located((By.XPATH, '/html/body/div/div/div[1]/video'))
+    )
+    print("video is present in the DOM now")
+
+    print(totalTime*percentage)
+    driver.execute_script("""
+        const xpath = "/html/body/div/div/div[1]/video";
+        const container = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+
+        container.currentTime = arguments[0]*arguments[1] - 2;
+    """, totalTime, percentage)
+
+    driver.switch_to.default_content()
+    #switching to h5p iframe
+    switchToFrame('h5p-iframe', "class")
+
+    quiz_button = WebDriverWait(driver, delay).until(
+        EC.element_to_be_clickable((By.XPATH, '/html/body/div/div/div[2]/div[2]/div'))
+    )
+    print("quiz button is present in the DOM now")
+
+    quiz_button.click()
+
+    time.sleep(0.5)
+
+    try:
+        # Wait for the element with the xpath of h5p-content-interaction/seekbar 
+        options = WebDriverWait(driver, delay).until(
+            EC.presence_of_element_located((By.XPATH, "/html/body/div/div/div[5]/div/div[2]/div/div[2]/ul"))
+        )
+        print("options are present in the DOM now")
+
+        options = driver.find_elements(By.CSS_SELECTOR, '.h5p-answer')
+
+        for option in options:
+            option.click()
+        
+        driver.find_element(By.CSS_SELECTOR, '.h5p-question-check-answer').click()
+        time.sleep(0.5)
+        correctOptions = []
+        options_1 = driver.find_elements(By.CSS_SELECTOR, '.h5p-answer')
+        for option in options_1:
+            if "h5p-correct" in option.get_attribute("class"):
+                correctOptions.append(option.text.split('\n')[0])
+
+        driver.find_element(By.CSS_SELECTOR, '.h5p-question-try-again').click()
+
+        options_2 = driver.find_elements(By.CSS_SELECTOR, '.h5p-answer')
+
+        for option in options_2:
+            if option.text.split('\n')[0] in correctOptions:
+                option.click()
+
+        time.sleep(0.5)
+        driver.find_element(By.CSS_SELECTOR, '.h5p-question-check-answer').click()
+        time.sleep(0.5)
+        driver.find_element(By.CSS_SELECTOR, '.h5p-question-iv-continue').click()
+        time.sleep(3)
+        driver.switch_to.default_content()
+
+    except TimeoutException:
+        print("options did not show up")
+        exit()
+
+
+
+
+
+
+
+    
 
--- naviagtion.js
+++
@@ -1,6 +0,0 @@
-//document.querySelectorAll('.video-stream')[0].duration
-
-let totalTime = 1106.661
-let percentage = [0.22802299999999998, 0.530777, 0.63402, 0.964188]
-let i = 0;
-document.querySelectorAll('.video-stream')[0].currentTime = totalTime*percentage[i] - 2
